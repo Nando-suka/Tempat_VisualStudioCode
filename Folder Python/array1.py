2279db06f8fa9fdc9ef7163e07273138e895a553
Fernando <117743517+Nando-suka@users.noreply.github.com>
2024-04-12 21:04:18 +0700

    meng-update file terbaru
    menambahkan perbaruan kepada file array1.py dan test3.py

--- Folder Python/array1.py
+++ Folder Python/array1.py
@@ -1,5 +1,10 @@
+import array
 # Mengakses nilai variabel array
 dataUmur = [35,23,20,19,11]
+print(dataUmur)
+dataUmur [1] = 25
+print(id(dataUmur[0]))
+print(id(dataUmur[1]))
 print(dataUmur)
 print(dataUmur[0])
 print(dataUmur[3])
@@ -19,21 +24,21 @@ print(dataUmur3)
 print(dataUmur3.insert(2,2))
 
 # Menggunakan inputan untuk menambahkan array
-n = int(input())
+""" n = int(input())
 a = []
 for i in range(n):
         a.append([int(j) for j in input().split()])
 print(a)
 
-print()
+print() """
 
-m = int(input("Masukkan jumlah baris: "))
+""" m = int(input("Masukkan jumlah baris: "))
 n = int(input("Masukkan jumlah kolom: "))
 
 print("Masukkan elemen array per baris, pisahkan dengan spasi:")
 array_2d = [list(map(int, input().strip().split())) for i in range(m)]
-
-print("\nArray 2 Dimensi yang Anda Masukkan:")
+ """
+""" print("\nArray 2 Dimensi yang Anda Masukkan:")
 for row in array_2d:
     print(row)
 
@@ -41,5 +46,31 @@ count = 9
 
 while (count < 9):
    print('The count is: ',count)
-   count = count + 1
+   count = count + 1 """
 
+
+# List Operations dalam Python
+tim = ['Chelsea','Arsenal','Brighton']
+
+print(tim.index('Chelsea'))
+tim.append('Newcastle')
+print(tim)
+tim.extend(['Madrid','Barcelona'])
+print(tim)
+tim.reverse()
+print(tim)
+tim.remove('Chelsea')
+print(tim)
+tim.clear()
+print(tim)
+
+""" Pemrosesan array merujuk pada operasi-operasi yang dilakukan pada elemen-elemen suatu array. 
+Operasi ini melibatkan manipulasi hingga pengolahan elemen yang ada pada array. 
+Adapun pemrosesan sekuensial adalah sebuah pemrosesan setiap elemen array yang dimulai dari 
+elemen pada indeks terkecil hingga terbesar. Pemrosesan sekuensial lebih sering menggunakan 
+pengulangan (loop/iterasi) dalam setiap prosesnya.
+ """
+# Menggunakan library array
+x = array.array("i",[1,2,3,4,5])
+print(x)
+print(type(x))
--- Folder Python/test3.py
+++ Folder Python/test3.py
@@ -1,7 +1,36 @@
-# Penangan Kesalahan (Error Handling dan Exception) dalam Python
+# Penangan Kesalahan (Error Handling dan Exception) dalam Python dan
+# Operasi pada List, Set, dan String
 z = 0
 try:
  print(1/z)
 except ZeroDivisionError:
  print("Anda tidak bisa membagi angka dengan nol")
 
+# Fungsi Set
+kumpulan = set([2,3,2,2,5,35,6,6,7,834,65,78,9,5,2])
+kumpulan_2 = [2,3,2,2,5,35,6,6,7,834,65,78,9,5,2]
+print(kumpulan)
+print(len(kumpulan))
+
+# Fungsi Min dan Max
+print(min(kumpulan),max(kumpulan))
+
+# Fungsi count pada Python
+print(kumpulan_2.count(2))
+
+# Fungsi count pada tipe data string
+klub = 'Chelsea Fc'
+cari = 'e'
+print(klub.count(cari))
+
+# Operator in dan not in untuk mencari data dan menghasilkan keluaran boolean
+kalimat = 'Benda itu jatuh entah kemana saat ku datang melihatnya'
+print('Benda' in kalimat) # True
+print('ini' in kalimat)
+print('aku' not in kalimat)
+print('melihatnya' not in kalimat)
+
+# Fungsi sort
+merek_laptop = ['dell','acer','asus','lenovo','hp','apple','axioo','avell']
+merek_laptop.sort()
+print(merek_laptop)

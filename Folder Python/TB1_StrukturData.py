130b6439624dbf99a3d1e2b86f29c981e48f030a
Fernando <117743517+Nando-suka@users.noreply.github.com>
2024-04-20 01:35:46 +0700

    Menambahkan file baru

---
+++ Folder Python/TB1_StrukturData.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+from aquarel import load_theme
+import numpy as np
+
+# Memasukkan theme dari Aquarel
+theme = load_theme("boxy_dark")
+theme.apply()
+
+# Data untuk box plot
+np.random.seed(10)
+data = [np.random.normal(0, std, 100) for std in range(1, 4)]
+
+# Membuat box plot
+plt.boxplot(data, patch_artist=True)
+
+# Menambahkan label sumbu x
+plt.xlabel('Data')
+
+# Menambahkan label sumbu y
+plt.ylabel('Nilai')
+
+# Menambahkan judul
+plt.title('Contoh Box Plot')
+
+# Menampilkan grid
+plt.grid(True)
+
+# Menampilkan plot
+plt.show()
+
+# Menampilkan theme
+theme.apply_transforms()    

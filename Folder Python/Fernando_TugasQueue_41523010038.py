dd7d736865a05f26f91e80487581444665a2481f
Fernando <117743517+Nando-suka@users.noreply.github.com>
2024-04-21 10:23:55 +0700

    Menambahkan pembaruan untuk file python

--- Folder Python/Fernando_TugasQueue_41523010038.py
+++ Folder Python/Fernando_TugasQueue_41523010038.py
@@ -1,7 +1,10 @@
 from collections import deque
 
 queue = deque()
-queue.append(1)
-queue.append(2)
-queue.append(3)
-print(queue.popleft())
+queue.append(10)
+queue.append(20)
+queue.append(30)
+queue.append(40)
+queue.append(50)
+print(queue.pop())
+
--- Folder Python/Fernando_TugasStack_41523010038.py
+++ Folder Python/Fernando_TugasStack_41523010038.py
@@ -12,9 +12,26 @@ class Stack:
     def is_empty (self):
         return len (self.items) == 0
     
+    def peek (self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def size (self):
+        return len(self.items)    
+
 # Contoh penggunaan stack
 stack = Stack()
-stack.push(1)
+stack.push(0)
+stack.push(9)
 stack.push(2)
-stack.push(3)
 print(stack.pop())
+print(stack.peek())
+print(stack.size())
+
+
+bl = (([(10,10,20),(40,20,10),(5,6,7)]))
+print(bl)
+print(bl[0])
+print(bl[0][2])
+bl.append([5,5,5])
+print(bl)

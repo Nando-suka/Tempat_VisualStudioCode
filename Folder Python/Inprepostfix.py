b6116a13c9f8162034582734d9368c52d93e6376
Fernando <117743517+Nando-suka@users.noreply.github.com>
2024-05-04 00:43:36 +0700

    Menambahkan file Python

---
+++ Folder Python/Inprepostfix.py
@@ -0,0 +1,22 @@
+def is_balanced (expression):
+    # Maping dari simbol penutup ke simbol terbuka
+    matching_bracket = {')':'(','}':'{',']':'['}
+    stack = []
+
+    for char in expression:
+        if char in matching_bracket.values():
+            stack.append(char)
+        elif char in matching_bracket:
+            if not stack or stack.pop() != matching_bracket[char]:
+                return False    
+            
+    return not stack
+
+# Menerima input langsung dari user
+
+
+
+
+
+user_input = input("Masukkan sebuah ekspresi untuk diperiksa apakah seimbang:")
+print("Balanced" if is_balanced(user_input) else "Not Balanced")

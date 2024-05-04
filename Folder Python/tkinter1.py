a8fe2238605f8855ac48f24cf01de131fdd622f5
Fernando <117743517+Nando-suka@users.noreply.github.com>
2024-05-04 22:37:50 +0700

    Menambahkan beserta mengupdate file Python

--- Folder Python/tkinter1.py
+++ Folder Python/tkinter1.py
@@ -4,6 +4,6 @@ from tkinter import ttk
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="Halo Dunia").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+ttk.Label(frm, text="Ini Fernando").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=4, row=5)
 root.mainloop()

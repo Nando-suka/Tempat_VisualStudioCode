a14d8915aa96a67be72d9b88e30415a9d6039bed
Fernando <117743517+Nando-suka@users.noreply.github.com>
2024-05-07 22:15:19 +0700

    Mengupdate file Python

--- Folder Python/objek1.py
+++ Folder Python/objek1.py
@@ -1,28 +1,46 @@
-class Triangle:
-    def __init__(self,points):
-        self.sides = 3
-        self.points = list(points)
-        if len(self.points) != 3:
-            raise ValueError ("Wrong number points")
-        
-    def sides (self):
-        return 3
+# Ini pembuatan kelas
+class Mobil:
+    # Ini pembuatan atribut untuk mobil
+    def __init__(self,merek,warna,kecepatan):
+        self.merek = merek
+        self.warna = warna
+        self.kecepatan = kecepatan
+
+    def tambahKecepatan (self):
+        self.kecepatan += 10
+
+
+# Proses Pemanggilan kelas
+Mobil1 = Mobil("Toyota","Hitam",100)
+print(Mobil1)
+print(Mobil1.kecepatan)
+Mobil1.tambahKecepatan()
+print(Mobil1.kecepatan)
+
+# Belajar memahami konsep private dan public 
+class Diary:
+    def __init__(self, title):
+        self.title = title
+        self._entries = []
+
+    def addentry (self, entry):
+        self._entries.append(entry)
+
+    def _lastentri (self):
+        return self._entries[-1]
     
-    def __str__(self):
-        return "I'm a triangle"
-    
-class Square:
-    def __init__(self,points):
-        self.sides = 4
-        self.points = list(points)
-        if len(self.points) != 4:
-            raise ValueError ("Wrong number points")
-        
-    def side (self):
-        return 4
-    
-    def __str__(self):
-        return "I'm so Square"
-        
-                                                                                                                                                                                                                                                                                                                                                                                            
-                                                                                                                                                                                                                                                                                                    
+# Proses pemanggilan kelas diary
+catatan = Diary("Menambahkan judul")
+catatan.addentry("Ini hari yang baik.")
+print("Catatan ini diperlihatkan ...",catatan.title)
+
+# Belajar menerapkan enkapsulasi pada python
+class tugas:
+    def __init__(self):
+        self._rahasia = "Fernando mencintai seseorang"
+
+    def __privateMessages (self):
+        print("Ini Pesan Rahasia")
+
+task1 = tugas()
+task1.__privateMessages()

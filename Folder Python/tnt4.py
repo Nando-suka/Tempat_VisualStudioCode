d03f700fd1afe5b006f803b35a20b73bf4261a0b
Fernando <117743517+Nando-suka@users.noreply.github.com>
2024-05-13 22:33:11 +0700

    Menambahkan file Python

---
+++ Folder Python/tnt4.py
@@ -0,0 +1,23 @@
+# Soal 1
+x = 1
+if x > 0:
+    print("Nilai %x adalah besar dari 0 " %x)
+
+# Soal 2
+nilai = 3
+if (nilai > 7):
+    print("Selamat Anda lulus")
+else:
+    print("Maaf Anfa itdak lulu")
+
+2
+# Soal 3
+number2 = 5
+if number2 < 0:
+    print("Negatif")
+else:
+    print()
+
+# Soal 4
+a = 5
+if  
